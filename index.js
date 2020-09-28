9b09d81dc3d28bf06418326518000671288c13ff
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-28 15:12:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(theNames, eventName) {
+    let theOutput = [];
+    for (let i = 0; i < theNames.length; i++) {
+        theOutput.push(`Thank you, ${theNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return theOutput;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
